7b1c1deb5a5c06596d542b9cf93c87988b55aff3
Martin Justa <Aztabanee@gmail.com>
2025-07-28 23:57:35 +0200

    added path helper functions

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: mjusta <mjusta@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 00:58:51 by mjusta            #+#    #+#             */
-/*   Updated: 2025/07/25 14:59:52 by mjusta           ###   ########.fr       */
+/*   Updated: 2025/07/28 23:53:48 by mjusta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,14 @@
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdlib.h>
+# include <stdio.h>
 # include <sys/wait.h>
 
-void	child_process1(char *infile, char *cmd, int pipefd[2], char **envp);
-void	child_process2(char *outfile, char *cmd, int pipefd[2], char **envp);
+void	infile_process(char *infile, char *cmd, int pipefd[2], char **envp);
+void	outfile_process(char *outfile, char *cmd, int pipefd[2], char **envp);
 
 void	error_exit(const char *msg);
-char	*get_env_path(char **envp);
+char	*find_cmd_path(char *cmd, char **envp);
 char	*join_path_cmd(char *path, char *cmd);
 
 #endif
