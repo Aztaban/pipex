725ac982233014cc556cb45d4339917dcf5b9bfd
Aztaban <Aztabanee@gmail.com>
2025-08-20 01:12:59 +0200

    modified header

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: mjusta <mjusta@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/05 00:58:51 by mjusta            #+#    #+#             */
-/*   Updated: 2025/08/20 00:30:14 by mjusta           ###   ########.fr       */
+/*   Updated: 2025/08/20 00:49:53 by mjusta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,10 @@ typedef struct s_pipex
 void	infile_process(char *infile, char *cmd, int *pipefd, char **envp);
 void	outfile_process(char *outfile, char *cmd, int *pipefd, char **envp);
 
-void	error_exit(const char *msg, int error_code);
 int		handle_children(pid_t pid2);
+void	handle_exec(char *cmd_path, char **cmd_args, char **envp);
+void	handle_invalid_cmd(char **cmd_args);
+void	error_exit(const char *msg, int error_code);
 char	*find_cmd_path(char *cmd, char **envp);
 
 #endif
